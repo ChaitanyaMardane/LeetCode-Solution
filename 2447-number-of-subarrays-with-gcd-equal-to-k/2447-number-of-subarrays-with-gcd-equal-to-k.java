9ed79858807bc761110542956ce78af7fdef586d
Chaitanya Mardane <142397032+ChaitanyaMardane@users.noreply.github.com>
2026-08-18 12:46:04 +0530

    Time: 7 ms (49.19%), Space: 43.7 MB (87.97%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
@@ -6,38 +6,30 @@ class Solution {
             for( int i =0 ; i < n ; i++){
                 g=arr[i];
                 for(int j = i ;j<n;j++)
-                {g=gcd(g,arr[j]);
-                if(g==k)cnt++;}
+                {
+                    g=gcd(g,arr[j]);
+                    
+                if (g < k || g % k != 0) 
+                    break;
+              
+                    if(g==k)cnt++;
+                }
             }
              System.out.println(cnt);
 
             return cnt;
         
-        //    return  count(arr,k);
        
     }
-
     public int gcd(int a , int b){
-        return b==0?a:gcd(b,a%b);
-    }
-    public int fact(int n){
-        if(n<=1)return n;
-        return n*fact(n-1);
-    }
-    public int count(int[] arr,int k){
-         int ans=0;
-         int n =arr.length;
-        for(int j = 0 ; j <n;j++){
-            int cnt = 0;
-            while(j<n && arr[j]!=1){
-                cnt++;
-                j++;
-            }
-
-            ans+=fact(cnt);
-
+        while (b!=0){
+            int r = a%b;
+            a=b;
+            b=r;
         }
-        return ans;
+        return a;
     }
+
+    
     
 }
